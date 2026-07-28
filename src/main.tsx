cdde57f709d83b5e8d8bf9420aa0c849d48fcf00
Mark Soyiri <marksoyiri001@gmail.com>
2026-07-28 18:48:34 +0000

    Fix React mount target: #root -> #app to match index.html

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
