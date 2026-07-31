05914d5688077de0222b84eeaabdb87e72f8fe8c
Mark Soyiri <marksoyiri001@gmail.com>
2026-07-31 09:49:48 +0000

    Fix admin pages overflowing viewport width on mobile

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -100,7 +100,7 @@ export default function AdminLayout() {
         </div>
       </aside>
 
-      <div className="flex-1 flex flex-col min-h-screen">
+      <div className="flex-1 flex flex-col min-h-screen min-w-0">
         <header className="h-16 glass border-b border-border flex items-center justify-between px-4 md:px-6 sticky top-0 z-30">
           <button
             onClick={() => setSidebarOpen(true)}
